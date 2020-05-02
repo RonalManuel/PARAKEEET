c717a41f97a9a025371fe61d8189dd20d4c35a2d
Ronal2710 <64109712+Ronal2710@users.noreply.github.com>
2020-05-02 15:41:51 -0500

    Add files via upload

---
+++ PARAKEEET-ronal_rama/Parakeet/src/main/java/pe/edu/upc/entity/Categoria_Producto.java
@@ -0,0 +1,26 @@
+package pe.edu.upc.entity;
+
+public class Categoria_Producto {
+	private int CCategoria;
+	private String NCategoria;
+	private String Descripcion;
+	public int getCCategoria() {
+		return CCategoria;
+	}
+	public void setCCategoria(int cCategoria) {
+		CCategoria = cCategoria;
+	}
+	public String getNCategoria() {
+		return NCategoria;
+	}
+	public void setNCategoria(String nCategoria) {
+		NCategoria = nCategoria;
+	}
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	
+}
---
+++ PARAKEEET-ronal_rama/Parakeet/src/main/java/pe/edu/upc/entity/Persona.java
@@ -0,0 +1,46 @@
+package pe.edu.upc.entity;
+
+public class Persona {
+private int CPersona;
+private String NPersona;
+private String NApellido;
+private String Correo;
+private int DNI;
+private String TDireccion;
+public int getCPersona() {
+	return CPersona;
+}
+public void setCPersona(int cPersona) {
+	CPersona = cPersona;
+}
+public String getNPersona() {
+	return NPersona;
+}
+public void setNPersona(String nPersona) {
+	NPersona = nPersona;
+}
+public String getNApellido() {
+	return NApellido;
+}
+public void setNApellido(String nApellido) {
+	NApellido = nApellido;
+}
+public String getCorreo() {
+	return Correo;
+}
+public void setCorreo(String correo) {
+	Correo = correo;
+}
+public int getDNI() {
+	return DNI;
+}
+public void setDNI(int dNI) {
+	DNI = dNI;
+}
+public String getTDireccion() {
+	return TDireccion;
+}
+public void setTDireccion(String tDireccion) {
+	TDireccion = tDireccion;
+}
+}
